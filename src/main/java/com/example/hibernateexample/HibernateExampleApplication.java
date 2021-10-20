9ec55063d6624b344482bb6fd074c985f5670e33
Sid <3923297-igdoma@users.noreply.gitlab.com>
2021-10-20 10:17:36 +0700

    update

--- src/main/java/com/example/hibernateexample/HibernateExampleApplication.java
+++ src/main/java/com/example/hibernateexample/HibernateExampleApplication.java
@@ -1,6 +1,8 @@
 package com.example.hibernateexample;
 
 import com.example.hibernateexample.entity.Student;
+import lombok.Cleanup;
+import org.osgi.util.converter.Converters;
 import org.hibernate.SessionFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,11 +12,22 @@ import org.hibernate.cfg.Configuration;
 public class HibernateExampleApplication {
 
     public static void main(String[] args) {
-//        SpringApplication.run(HibernateExampleApplication.class, args);
-        SessionFactory sessionFactory = new Configuration()
-                .configure("hibernate.cfg.xml")
-                .addAnnotatedClass(Student.class)
-                .buildSessionFactory();
+//        var context = SpringApplication.run(HibernateExampleApplication.class, args);
+        var session = sessionFactory.getCurrentSession();
+
+        //Create student obj
+        System.out.println("Creating Student");
+        var student = new Student("Ahmad", "Zidan", "lan666as@mail.com");
+
+        //Start transaction
+        session.beginTransaction();
+
+        //Save student obj
+        System.out.println("Saving");
+        session.save(student);
+        session.getTransaction().commit();
+
+        System.out.println("Done");
     }
 
 }
